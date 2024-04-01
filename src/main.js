6517247fe58d45012129bf130168eeed40d5a51a
Tran Chau Phu <ilovewhenifyoucallme@gmail.com>
2024-04-01 21:04:58 +0700

    b_12

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import Default from './Layout/Wrapper/index.vue'
+import Default from './layout/wrapper/index.vue'
+import Toaster from "@meforma/vue-toaster";
+
 const app = createApp(App)
 
-app.use(router)
+app.use(router, Toaster)
 app.component("default-layout", Default);
 
 app.mount("#app")
